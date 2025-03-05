f6ec1b1b611036983dc5bfcbd8aeebbd1b4471c5
mrheathv <73443831+mrheathv@users.noreply.github.com>
2025-03-05 14:48:08 -0600

    Update test.py

--- test.py
+++ test.py
@@ -9,14 +9,17 @@ def get_application_data():
     query = """
     SELECT 
         dim_company.name AS Company_Name,
+        dim_role.role AS Role,
         dim_status.status AS Status
     FROM fact_application
     JOIN dim_company ON fact_application.company_id = dim_company.id
+    JOIN dim_role ON fact_application.role_id = dim_role.id
     JOIN dim_status ON fact_application.status_id = dim_status.id
     """
     df = pd.read_sql(query, conn)
     conn.close()
     return df
+
 
 st.title("Applications List")
 
